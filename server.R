11a807963b7f6bb166da93611b3c517bc2bd835e
RemyNicolle <remy.c.nicolle@gmail.com>
2017-08-31 08:53:05 +0200

    added info to img legends

--- server.R
+++ server.R
@@ -13,7 +13,7 @@ if(haslabel){
         LABELS= paste0(samp[,"sample"]," : ",apply(samp[,labelcol],1,paste,collapse=" "))
     }
 }else{
-    LABELS=paste0(samp[,"sampleID"]," (",samp[,"whole.tumor.classification"],")")
+    LABELS=paste0(samp[,"sampleID"]," (subtype: ",samp[,"whole.tumor.classification"],")")
 }
 
 
@@ -29,13 +29,20 @@ shinyServer(function(input, output, session) {
     
     
    selectImageAndOrder=reactive({
+       
+       
         selcomp=input$comp
+        
+        rang=signif(range(samp[,selcomp],na.rm=T),3)
+        rangstr=paste( "range: [",rang[1],",",rang[2],"]")
+        
         imgdir=imgdirs[[input$which]]
         ORD=order(samp[,selcomp],decreasing=T)
         list(
                 size=input$size,
         imgpath=paste0(imgdir,samp[ORD,"histologyID"],imgextension),
-        labels=paste(LABELS[ORD],selcomp,":",signif(samp[ORD,selcomp],3))
+        labels=paste(LABELS[ORD],selcomp,":",signif(samp[ORD,selcomp],3),
+        " ( percentile:",signif(100*((N:1)/N),2),"% ;", rangstr ,")")
         )
     })
     
