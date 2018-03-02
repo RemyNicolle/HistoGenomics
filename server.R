886340b5d69a8ffcf7694239730ce4baf786c798
Remy <remy.c.nicolle@gmail.com>
2018-03-02 15:58:53 +0100

    Modified entire app behavior to only show ONE SLIDE AT THE TIME ! Makes the entire app snappier.

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ comps=grep("comp",colnames(samp),value=T)
 
 
 
+
 haslabel=length(grep("label",colnames(samp)))>0
 if(haslabel){
     labelcol=grep("label",colnames(samp),value=T)
@@ -18,46 +19,84 @@ if(haslabel){
 
 
 imgdirs=list(selected="dataimg/selected/"
-,full="dataimg/full/")
+    ,full="dataimg/full/")
 imgextension=".jpg"
 
 
 
 
 shinyServer(function(input, output, session) {
-    
-    
-    
-   selectImageAndOrder=reactive({
-       
-       
-        selcomp=input$comp
-        
-        rang=signif(range(samp[,selcomp],na.rm=T),3)
-        rangstr=paste( "range: [",rang[1],",",rang[2],"]")
-        
-        imgdir=imgdirs[[input$which]]
-        ORD=order(samp[,selcomp],decreasing=T)
-        list(
-                size=input$size,
-        imgpath=paste0(imgdir,samp[ORD,"histologyID"],imgextension),
-        labels=paste(LABELS[ORD],selcomp,":",signif(samp[ORD,selcomp],3),
-        " ( percentile:",signif(100*((N:1)/N),2),"% ;", rangstr ,")")
-        )
-    })
-    
-    lapply(1:N, function(i) {
-        output[[paste0('plot', i)]] <- renderImage({
-            
-            list(src = selectImageAndOrder()$imgpath[[i]],
-            hover=selectImageAndOrder()$labels[[i]],
-            #width=300,height=200,
-            width=paste0(selectImageAndOrder()$size,"%"),
-            alt = "Missing img")
-        },deleteFile=F)
-        
-        output[[paste0('lab', i)]]<-renderText({
-        selectImageAndOrder()$labels[[i]]
-        })
-    })
+
+   getCurrentRank=reactive({
+    i=    input$rank
+    # print(i)
+    if(is.null(i)|is.na(i)){
+        return(NA)
+    }else if(i<1){
+        updateNumericInput(session,"rank",value=1);
+        return(1)
+    }else if(i>N){
+       updateNumericInput(session,"rank",value=N);
+       return(N)
+   }
+   input$rank
 })
+
+   observeEvent(input$comp, {
+      updateNumericInput(session,"rank",value=1);
+
+  })
+
+
+   observeEvent(input$previous, {
+      # print(getCurrentRank())
+      if(is.na(getCurrentRank())|is.null(getCurrentRank())){
+          updateNumericInput(session,"rank",value=1);
+      }else if(getCurrentRank()>1){
+          updateNumericInput(session,"rank",value=getCurrentRank()-1);
+      }
+  })
+   observeEvent(input$nex, {
+       # print(getCurrentRank())
+       if(is.na(getCurrentRank())|is.null(getCurrentRank())){
+          updateNumericInput(session,"rank",value=1);
+       }else if(getCurrentRank()<N){
+          updateNumericInput(session,"rank",value=getCurrentRank()+1);
+       }
+  })
+
+   output$plot <- renderImage({
+    list(src = selectImageAndOrder()$imgpath,
+        hover=selectImageAndOrder()$labels,
+        width=paste0(selectImageAndOrder()$size,"%"),
+        contentType="image/jpeg",
+        alt = "Missing img")
+},deleteFile=F)
+
+
+   output$lab<-renderText({selectImageAndOrder()$labels})
+
+   selectImageAndOrder=reactive({
+
+     i=getCurrentRank()
+     if(is.null(i)|is.na(i))return(list(imgpath="",labels="Wrong rank",size=20));
+     selcomp=input$comp
+
+     rang=signif(range(samp[,selcomp],na.rm=T),3)
+     rangstr=paste( "range: [",rang[1],",",rang[2],"]")
+
+     imgdir=imgdirs[[input$which]]
+     ORD=order(samp[,selcomp],decreasing=T)
+     list(
+        size=input$size,
+        imgpath=paste0(imgdir,samp[ORD,"histologyID"],imgextension)[i],
+        labels=paste(LABELS[ORD],selcomp,":",signif(samp[ORD,selcomp],3),
+            " ( rank:",i,"; percentile:",signif(100*((N:1)/N),2),"% ;", rangstr ,")")[i]
+        )
+ })
+
+
+
+
+})
+
--- ui.R
+++ ui.R
@@ -6,36 +6,42 @@ N=nrow(samp)
 comps=colnames(samp)[c(3:10,14:38)]
 
 fluidPage(
-title = 'Histogenomics',
+	title = 'Histogenomics',
 
 #sidebarLayout(
 #sidebarPanel(
 # create some select inputs
 
-sliderInput("size", "Image size (%)", min = 10, max = 150, value = 80),
+	sliderInput("size", "Image size (%)", min = 10, max = 150, value = 80),
 
-radioButtons("which","Show zoom on selected portion of the slide or full slide:", choiceValues=c("selected","full"),
-choiceNames=c("10x zoom on selection","full slide"),
-selected="full"),
+	radioButtons("which","Show zoom on selected portion of the slide or full slide:", choiceValues=c("selected","full"),
+		choiceNames=c("10x zoom on selection","full slide"),
+		selected="full"),
 
-selectInput("comp","Select component:",
-choices = comps),
+	selectInput("comp","Select component:",
+		choices = comps),
 
 
 #),
 
 #mainPanel(
 
+h4("Sample selection"),
+	fluidRow(
+		column(1,	actionButton("previous", "previous")),
+	column(1,	numericInput("rank", label = NULL, value = 1,min=1,max=N,step=1,width="100px")),
+	column(1,actionButton("nex", "next"))
+	),
 
 # UI output
-lapply(1:N, function(i) {
+# lapply(1:N, function(i) {
     #    plotOutput(paste0('plot', i),height="100%")
-    fluidRow(
-         textOutput(paste0('lab', i)),
-    imageOutput(paste0('plot', i),height="auto",width="auto")
+    # fluidRow(
+	textOutput(paste0('lab')),
+	imageOutput(paste0('plot'),height="auto",width="auto")
 
-    )
-})
+	# )
+# })
 #)
 #)
 #)
